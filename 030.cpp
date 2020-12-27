fa56999dd359a88d021ff8d54e9ef7f901c049a2
Husnain Raza <hhrraazzaa@gmail.com>
2020-12-26 19:46:13 -0600

    Problem 30

---
+++ 030.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int out = 0;
+	for (int i = 0; i < 6*pow(9,5); i++) {
+		std::cout << i << std::endl;
+	}
+	return 0;
+}
